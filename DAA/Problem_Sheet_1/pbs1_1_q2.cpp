920e07b13a6603036f1e68fbc9db12000f6f78d1
Aryan Vishwakarma <aryan.vishwakarma2020@vitstudent.ac.in>
2021-09-13 14:05:16 +0530

    question added

--- DAA/Problem_Sheet_1/pbs1_1_q2.cpp
+++ DAA/Problem_Sheet_1/pbs1_1_q2.cpp
@@ -1,3 +1,15 @@
+/*
+Question
+Counting inversions
+Inversion Count for an array indicates – how far (or close) the array is from being sorted. If array 
+is already sorted then inversion count is 0. If array is sorted in reverse order that inversion count 
+is the maximum. Formally speaking, two elements a[i] and a[j] form an inversion if a[i] > a[j] 
+and i < j.
+Example: Input: arr[] = {8, 4, 2, 1}
+Output: 6. The six inversions: (8,4), (4,2), (8,2), (8,1), (4,1), (2,1).
+Input: arr[] = {3, 1, 2}
+Output: 2. The two inversions: (3,1), (3,2).
+*/
 #include <iostream>
 using namespace std;
 int main()
